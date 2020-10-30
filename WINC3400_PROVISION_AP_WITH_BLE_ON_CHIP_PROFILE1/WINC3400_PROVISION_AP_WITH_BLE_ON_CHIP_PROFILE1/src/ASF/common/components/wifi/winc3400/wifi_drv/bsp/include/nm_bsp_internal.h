49e7086aab266dcb751ae1267152bb5b84dbde4c
BELAP84376\PTSD <15479248+davepruitt@users.noreply.github.com>
2020-10-30 14:52:49 +0100

    Bringing in the Arduino version of nm_bsp_internal.h

--- WINC3400_PROVISION_AP_WITH_BLE_ON_CHIP_PROFILE1/WINC3400_PROVISION_AP_WITH_BLE_ON_CHIP_PROFILE1/src/ASF/common/components/wifi/winc3400/wifi_drv/bsp/include/nm_bsp_internal.h
+++ WINC3400_PROVISION_AP_WITH_BLE_ON_CHIP_PROFILE1/WINC3400_PROVISION_AP_WITH_BLE_ON_CHIP_PROFILE1/src/ASF/common/components/wifi/winc3400/wifi_drv/bsp/include/nm_bsp_internal.h
@@ -38,67 +38,7 @@
 #ifndef _NM_BSP_INTERNAL_H_
 #define _NM_BSP_INTERNAL_H_
 
-
-
-#ifdef WIN32
-#include "nm_bsp_win32.h"
-#endif
-
-#ifdef __K20D50M__
-#include "nm_bsp_k20d50m.h"
-#endif
-
-#ifdef __MSP430FR5739__
-#include "bsp_msp430fr5739.h"
-#endif
-
-#ifdef _FREESCALE_MCF51CN128_
-#include "bsp/include/nm_bsp_mcf51cn128.h"
-#endif
-
-#ifdef __MCF964548__
-#include "bsp/include/nm_bsp_mc96f4548.h"
-#endif
-
-#ifdef __APP_APS3_CORTUS__
-#include "nm_bsp_aps3_cortus.h"
-#endif
-
-#if (defined __SAMD21J18A__) || (defined __SAMD21G18A__)
 #include "bsp/include/nm_bsp_samd21.h"
-#include "bsp/include/nm_bsp_samd21_app.h"
-#endif
-
-#if (defined __SAM4S16C__) || (defined __SAM4SD32C__)
-#include "bsp/include/nm_bsp_sam4s.h"
-#include "bsp/include/nm_bsp_sam4s_app.h"
-#endif
-
-#ifdef __SAMG55J19__
-#include "bsp/include/nm_bsp_samg55.h"
-#include "bsp/include/nm_bsp_samg55_app.h"
-#endif
-
-#ifdef __SAMG53N19__
-#include "bsp/include/nm_bsp_samg53.h"
-#endif
-
-#if (defined __SAME70Q21__) || (defined __SAME70Q21B__)
-#include "bsp/include/nm_bsp_same70.h"
-#include "bsp/include/nm_bsp_same70_app.h"
-#endif
-
-#ifdef CORTUS_APP
-#include "crt_iface.h"
-#endif
-
-#ifdef NRF51
-#include "nm_bsp_nrf51822.h"
-#endif
-
-#ifdef _ARDUINO_UNO_
-#include "bsp/include/nm_bsp_arduino_uno.h"
-#endif
-
+#define CONF_PERIPH
 
 #endif //_NM_BSP_INTERNAL_H_

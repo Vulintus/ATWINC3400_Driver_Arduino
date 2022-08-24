98bd37bc81c577cb1c191b98e391c242137fac02
David Pruitt <15479248+davepruitt@users.noreply.github.com>
2022-08-24 13:53:34 -0600

    Updated the example sketches

--- atwinc3400_test_01_ap/atwinc3400_test_01_ap.ino
+++ atwinc3400_test_01_ap/atwinc3400_test_01_ap.ino
@@ -1,21 +1,7 @@
-#include <WiFi101.h>
+#include <Vulintus_WiFi3400.h>
 
-/*
- * These pinouts assume the following:
- * 1. The board you are using is the SAMD21 Xplained Pro with a SAMD21J18A
- * 2. The ATWINC3400 is on a WINC3400 Xplained Pro board attached to EXT1.
- * 3. You have installed the SAMD21 Xplained Pro board package for the Arduino IDE
- *      from the AtmelUniversityFrance Github page. The URL is here:
- * https://github.com/AtmelUniversityFrance/atmel-samd21-xpro-boardmanagermodule/wiki/GettingStarted:-SAMD21-Xplained-Pro
- * 
- * If the above conditions are not true, the pinouts should be changed to match
- * your setup.
- */
-#define WINC_IRQ          	6     		//Digital input for the ATWINC3400 interrupt output.
-#define WINC_SS 			PIN_SPI_SS  //Digital output for the ATWINC3400 SPI slave select.	
-#define WINC_RESET        	A1          //Digital output for the ATWINC3400 reset input.
-#define WINC_EN		       	7           //Digital output for the ATWINC3400 enable input.
-#define WINC_WAKE			A0			//Digital output for the ATWINC3400 wake input.
+#define BOARD_NAME      VULINTUS_HABITRAK_V1
+#include "board_pin_definitions.h"
 
 WiFiServer server(80);
 uint8_t status = 0;
---
+++ atwinc3400_test_02_ble_init/board_pin_definitions.h
@@ -0,0 +1,40 @@
+#ifndef __BOARD_PIN_DEFINITIONS_H
+#define __BOARD_PIN_DEFINITIONS_H
+
+#ifndef BOARD_NAME
+#define BOARD_NAME              SAMD21_XPLAINED_PRO
+#endif
+
+#if BOARD_NAME == SAMD21_XPLAINED_PRO
+
+/*
+ * These pinouts assume the following:
+ * 1. The board you are using is the SAMD21 Xplained Pro with a SAMD21J18A
+ * 2. The ATWINC3400 is on a WINC3400 Xplained Pro board attached to EXT1.
+ * 3. You have installed the SAMD21 Xplained Pro board package for the Arduino IDE
+ *      from the AtmelUniversityFrance Github page. The URL is here:
+ * https://github.com/AtmelUniversityFrance/atmel-samd21-xpro-boardmanagermodule/wiki/GettingStarted:-SAMD21-Xplained-Pro
+ * 
+ * If the above conditions are not true, the pinouts should be changed to match
+ * your setup.
+ */
+//#define WINC_IRQ          	6     		//Digital input for the ATWINC3400 interrupt output.
+//#define WINC_SS 			PIN_SPI_SS  //Digital output for the ATWINC3400 SPI slave select.	
+//#define WINC_RESET        	A1          //Digital output for the ATWINC3400 reset input.
+//#define WINC_EN		       	7           //Digital output for the ATWINC3400 enable input.
+
+#define WINC_IRQ          	9     		//Digital input for the ATWINC3400 interrupt output.
+#define WINC_SS 			10  //Digital output for the ATWINC3400 chip select.	
+#define WINC_RESET        	7          //Digital output for the ATWINC3400 reset input.
+#define WINC_EN		       	8           //Digital output for the ATWINC3400 enable input.
+
+#elif BOARD_NAME == VULINTUS_HABITRAK_V1
+
+#define WINC_IRQ          	9     		//Digital input for the ATWINC3400 interrupt output.
+#define WINC_SS 			10  //Digital output for the ATWINC3400 chip select.	
+#define WINC_RESET        	7          //Digital output for the ATWINC3400 reset input.
+#define WINC_EN		       	8           //Digital output for the ATWINC3400 enable input.
+
+#endif
+
+#endif /* __BOARD_PIN_DEFINITIONS_H */
